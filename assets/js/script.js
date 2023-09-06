76b6ac69d0aadfcc2f79a7480faa523520ba79e1
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-09-06 13:55:28 -0700

    updated JavaScript file and linked to HTML, safe revert point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 const myKey = config.APIkey;
-const requestUrl = ''
+
+// Grab users location:
+navigator.geolocation.getCurrentPosition(position => {
+    const userLocation = position.coords;
+    // Show a map centered at latitude / longitude.
+    console.log(userLocation);
+    return(userLocation);
+});
+
+
+
+// Use zipcode as location: 
+
+
+// Location recognition Url (to get list of entities given lat & long)
+const requestUrl = 'https://dev.virtualearth.net/REST/v1/LocationRecog/' + userLocation + '?&top={10}&distanceunit=mi&verboseplacenames=false&includeEntityTypes=naturalPOI&key=' + myKey;
