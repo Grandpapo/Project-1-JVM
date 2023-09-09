6bcdbf4a463b58d32a965993b9650f7ea6e661ed
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-09-08 17:51:28 -0700

    updated JavaScript files and html, safe revert point

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,55 +4,15 @@ let advancedButtonEl = document.querySelector('.advanced-btn');
 let buttonContainerEl = document.querySelector('.button-container');
 let resultsEl = document.querySelector('.results');
 let userInputEl = document.querySelector('#user-input');
-let searchBtnEl = document.querySelector('.search-btn');
 
-
-// Grab users location using HTML Geolocation API:
-
-function initApp() {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const userCoords = position.coords;
-        // display user geolocation results:
-        const userLat = position.coords.latitude;
-        const userLon = position.coords.longitude;
-        // if user accepts geolocation, then use their lat and lon in google maps API
-        if (userCoords !== null) {
-            const userLocation = new google.maps.LatLng(userLat, userLon);
-            console.log(userLat);
-            console.log(userLon);
-            // looking for beaches within 30km (30000 m)
-            const request = {
-                location: userLocation,
-                radius: '30000',
-                keyword: 'beach'
-            }
-            const service = new google.maps.places.PlacesService(document.createElement('div')); // dummy element, since we don't want map
-            // nearbySearch request using Places API
-            service.nearbySearch(request, callback);
-            // grab latitude and longitude of all beach results from data output
-            function callback(results, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    const beachLocations = results.map(place => ({
-                        name: place.name,
-                        latitude: place.geometry.location.lat(),
-                        longitude: place.geometry.location.lng()
-                    }));
-                    console.log(beachLocations);
-                }
-            }
-        }
-    });
-}
-initApp();
-// once lat and lon are retrieved from google api, input the coords into the NOAA API to grab wave height, weather and wind
-// forEach item that is returned from google, run through NOAA API
+// once lat and lon are retrieved from google api, input the coords into the Weather API to grab wave height, weather and wind
+// forEach item that is returned from google, run through Weather API
 // if beginner is clicked, display locations with 1-2ft wave height
 // if intermediate is clicked, display locations with 3-5ft wave height
 // if advanced is clicked, display locations with 6-11ft wave height
 
+// Once user enters zipcode, grab lat and lon related to zipcode
 
-// If user doesn't share location, they can enter zipcode
-// turn zipcode into latitude and longitude
 function getCoordsByZip(zipcode, callback) {
 
     var geocoder = new google.maps.Geocoder();
@@ -76,7 +36,7 @@ function findNearbyBeaches(zipcode) {
     getCoordsByZip(zipcode, function (coords) {
         if (coords) {
             const userLocation = new google.maps.LatLng(coords.latitude, coords.longitude);
-            // looking for beaches within 30km (30000 m)
+            // looking for locations with keyword of beache within 30km (30000 m)
             const request = {
                 location: userLocation,
                 radius: '30000',
@@ -86,7 +46,7 @@ function findNearbyBeaches(zipcode) {
             const service = new google.maps.places.PlacesService(document.createElement('div')); // dummy element, since we don't want map
             // nearbySearch request using Places API
             service.nearbySearch(request, function (results, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) { // if lat & lon are found in google api w/o errors, return data as an array of objects
                     const beachLocations = results.map(place => ({
                         name: place.name,
                         latitude: place.geometry.location.lat(),
@@ -103,28 +63,83 @@ function findNearbyBeaches(zipcode) {
     });
 }
 
+// Grab users location using HTML Geolocation API:
 
-searchBtnEl.addEventListener('click', function (event) {
-    event.preventDefault();
-    var userInputZip = userInputEl.value;
-    userInputEl.textContent = '';
-    findNearbyBeaches(userInputZip);
-});
+function initApp() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const userCoords = position.coords;
+        // display user geolocation results:
+        const userLat = position.coords.latitude;
+        const userLon = position.coords.longitude;
+        // if user accepts geolocation, then use their lat and lon in google maps API
+        if (userCoords !== null) {
+            buttonContainerEl.classList.add('hide'); // hide first screen
+            resultsEl.classList.remove('hide'); // show second screen
+            const userLocation = new google.maps.LatLng(userLat, userLon);
+            console.log(userLat);
+            console.log(userLon);
+            // looking for locations with keyword of beache within 30km (30000 m)
+            const request = {
+                location: userLocation,
+                radius: '30000',
+                keyword: 'beach'
+            }
+            const service = new google.maps.places.PlacesService(document.createElement('div')); // dummy element, since we don't want map
+            // nearbySearch request using Places API
+            service.nearbySearch(request, callback);
+            // grab latitude and longitude of all beach results from data output
+            function callback(results, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) { // if lat & lon are found in google api w/o errors, return data as an array of objects
+                    const beachLocations = results.map(place => ({
+                        name: place.name,
+                        latitude: place.geometry.location.lat(),
+                        longitude: place.geometry.location.lng()
+                    }));
+                    console.log(beachLocations);
+                }
+            }
+        }
+    });
+}
+
+// If a zip code is entered, and a skill level is clicked, load in second screen and information for beaches
+// if no zip code is entered and a skill level is clicked, ask to use user location then load information for beaches
 
 beginnerButtonEl.addEventListener('click', function (event) {
     event.preventDefault();
+    console.log(userInputEl.value);
+    if (userInputEl.value !== '') {
+    var userInputZip = userInputEl.value;
+    userInputEl.textContent = '';
+    findNearbyBeaches(userInputZip);
     buttonContainerEl.classList.add('hide'); // hide first screen
     resultsEl.classList.remove('hide'); // show second screen
-});
+    } else {
+    initApp(); // ask for user location if no zipcode is entered
+}});
 
 intermediateButtonEl.addEventListener('click', function (event) {
     event.preventDefault();
+    console.log(userInputEl.value);
+    if (userInputEl.value !== '') {
+    var userInputZip = userInputEl.value;
+    userInputEl.textContent = '';
+    findNearbyBeaches(userInputZip);
     buttonContainerEl.classList.add('hide'); // hide first screen
-    resultsEl.classList.remove('hide');// show second screen
-});
+    resultsEl.classList.remove('hide'); // show second screen
+    } else {
+    initApp(); // ask for user location if no zipcode is entered
+}});
 
 advancedButtonEl.addEventListener('click', function (event) {
     event.preventDefault();
+    console.log(userInputEl.value);
+    if (userInputEl.value !== '') {
+    var userInputZip = userInputEl.value;
+    userInputEl.textContent = '';
+    findNearbyBeaches(userInputZip);
     buttonContainerEl.classList.add('hide'); // hide first screen
-    resultsEl.classList.remove('hide');// show second screen
-});
+    resultsEl.classList.remove('hide'); // show second screen
+    } else {
+    initApp(); // ask for user location if no zipcode is entered
+}});
